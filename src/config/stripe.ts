fa8def4f0a0b3eb35bfdbbf2440594509d17a243
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-10-21 16:20:41 +0200

    encore un fix de stripe

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,6 +1,6 @@
 export const STRIPE_PLAN = {
   name: "Abonnement Premium",
-  price: "price_1QCLbPGsj827lzCXQM6uuIqz",
+  price: "price_1QCMRkGsj827lzCXdCEaqa41",
   features: [
     "Accès à toutes les fonctionnalités de l'application",
     "Création illimitée de systèmes",
