e4c4e638ec4f0c04a4b18c42d5d00838a3b7395d
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-10-21 14:53:24 +0200

    fix : test stripe 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+  },
 };
 
 export default nextConfig;
