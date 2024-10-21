49a9dcc6cdf42caf68f3087f52c76ab4268920d6
ThomasLekieffre <thomaslekieffre59.dev@gmail.com>
2024-10-21 17:16:31 +0200

    fix: stripe

--- src/config/stripe.ts
+++ src/config/stripe.ts
@@ -1,7 +1,7 @@
 export const STRIPE_PLANS = {
   BASIC: {
     name: "Basic",
-    price: "price_1QCMRkGsj827lzCXdCEaqa41",
+    price: "price_1QCNKpGsj827lzCXSi0gg5xw",
     features: [
       "Fonctionnalité 1",
       "Fonctionnalité 2",
